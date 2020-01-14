e3e0cfc22474c42fc8a3be3d38546371d6617f96
NicolaWinterson <nicola.l.winterson@gmail.com>
2020-01-14 10:56:59 +0100

    Updated files

--- Projects and Snippets/UI emulator - Incredible Types/script.js
+++ Projects and Snippets/UI emulator - Incredible Types/script.js
@@ -1,2 +1,22 @@
+/*document.getElementById('fullscreen-menu').onclick = function() {
+    const toggleMenu = document.getElementById("hidden-nav");
+    toggleMenu.style.display = toggleMenu.style.display === 'none' ? 'block' : 'none';
+  };*/
+
+
+  document.getElementById('fullscreen-menu').onclick = function() {
+    const toggleMenu = document.getElementById("hidden-nav");
+    const menuInnerHTML = document.getElementById('fullscreen-menu');
+    if (toggleMenu.style.display === "none") {
+        toggleMenu.style.display = "block";
+        menuInnerHTML.innerHTML = "Hide menu"
+        console.log("toggle show menu")
+    } else {
+        toggleMenu.style.display = "none";
+        menuInnerHTML.innerHTML = "Show menu"
+        console.log("toggle hide menu")
+    }
+  }
+
 
 
