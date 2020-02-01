746780e4ce105377ab039e4eb7f805f3cccf3c59
NicolaWinterson <nicola.l.winterson@gmail.com>
2020-02-01 14:24:18 +0100

    Removed readme file'

--- Projects and Snippets/Recipe Search/src/App.js
+++ Projects and Snippets/Recipe Search/src/App.js
@@ -43,9 +43,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <header className="header">
-        <h1>Recipe Search</h1>
-      </header>
+        <h1 className="header">Recipe Search</h1>
       <form onSubmit={getSearch} className="search-form">
         <input className="search-bar" type="text" value={search} onChange={updateSearch} />
         <button className="search-button" type="submit">
