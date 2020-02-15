96dfc45a08911eb33ab4c5f0aa8d69131c738fe2
Nicola Winterson <nicola.l.winterson@gmail.com>
2020-02-15 11:45:01 +0100

    Moved files

--- Projects and Snippets/C.V.React-Component/cv-app/src/components/TextList/ListInfo.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ListInfo = {
-  listitems: ["sing", "run", "swim"]
-};
-
-export default ListInfo;
--- Projects and Snippets/C.V.React-Component/cv-app/src/components/TextList/index.js
+++
@@ -1,4 +0,0 @@
-// import { TextList } from "../TextList";
-
-export { default as TextList } from "./TextList.jsx";
-export { default as styles } from "./styles.css";
